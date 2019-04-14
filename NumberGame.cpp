3753fcc5775357e44832a2e237220baed711be4d
IrfanAfa <irfanecn@gmail.com>
2019-04-14 16:22:34 +0200

    Added Computer againsts human

--- NumberGame.cpp
+++ NumberGame.cpp
@@ -7,16 +7,20 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
+
+  srand(time(0)); //synchronize the time to zero
+  const bool token = (rand() % 2);
   // if no arguments, the human tries to guess
-  const bool human_guess = 1;
+  bool human_guess = token;
   unsigned int m; //human entry
   int i = 1;      //no of chance
 
   if(human_guess)
   {
-    srand(time(0)); //synchronize the time to zero
+
+    cout << "Human Play against Computer" << endl;
     const unsigned int n = (rand() % 100);
-    cout << "Random number: " << n << '\n';
+    //cout << "Random number: " << n << '\n';
     while(true)
     {
       // Ask human to enter number
@@ -33,6 +37,39 @@ int main(int argc, char ** argv)
       }
       else{
         cout << "you have entered a large number" << endl;
+        i++;
+      }
+    }
+  }
+  else{
+
+    cout << "Computer Play against Human" << endl;
+    unsigned int lower = 1, upper = 100;
+    unsigned int guess, response;
+    while(true)
+    {
+      guess = 0.5*(lower + upper);
+      cout << " 0 if correct, 1 if small, 2 if large " << endl;
+      cout << " Is it " << guess << " ? ";
+      cin >> response;
+      if(response == 0)
+      {
+        cout << "Yes I Got you in " << i << " chance!!! "<< endl;
+        break;
+      }
+      else if(lower >= upper)
+      {
+        cout << "you are cheating" << endl;
+        break;
+      }
+      else if(response == 1)
+      {
+        lower = guess + 1;
+        i++;
+      }
+      else
+      {
+        upper = guess - 1;
         i++;
       }
     }
